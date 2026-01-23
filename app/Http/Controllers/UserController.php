d1f5fc8381f893e3cc11e57cfa84bfe8d895fe00
Yash Joshi <meyashjoshi@outlook.in>
2026-01-23 10:17:08 +0530

    Added Remining UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+
+    public function listUsers()
+    {
+    }
+
+    public function getUserDetails()
+    {
+    }
+
+    public function manageUser()
+    {
+    }
+
+    public function deleteUser()
+    {
+    }
+
+}
