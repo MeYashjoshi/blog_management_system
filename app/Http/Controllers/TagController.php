9b27c73c0c964a05b5065d7c8a3ac4593b6d5751
Yash Joshi <meyashjoshi@outlook.in>
2026-01-23 10:16:20 +0530

    Added new files with name correction

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+
+    public function listTags()
+    {
+    }
+
+    public function getTagDetails()
+    {
+    }
+
+    public function manageTag()
+    {
+    }
+
+    public function deleteTag()
+    {
+    }
+}
