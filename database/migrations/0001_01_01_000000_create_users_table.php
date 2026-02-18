ea54a8ec056a5060146d6986279ca73f030973ae
Yash Joshi <meyashjoshi@outlook.in>
2026-02-18 15:33:21 +0530

    Database: Update users table migration
    - Changed default value of 'status' column from '1' (Active) to '0' (Pending) to ensure new signups require approval.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,11 +19,11 @@ return new class extends Migration
             $table->string('password');
             $table->string('profile')->nullable();
             $table->text('bio')->nullable();
-            $table->enum('status', ['0', '1', '2'])->default('1')->comment('0=Pending, 1=Active, 2=Inactive');
+            $table->enum('status', ['0', '1', '2'])->default('0')->comment('0=Pending, 1=Active, 2=Inactive');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
-            });
+        });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
