07389cfe28ea3847b37b8d10b1777dc6bc0f1c79
Yash Joshi <meyashjoshi@outlook.in>
2026-02-20 11:50:07 +0530

    fix: Update search functionality to reset data on empty input

--- resources/views/Dashboard/userrequests.blade.php
+++ resources/views/Dashboard/userrequests.blade.php
@@ -215,7 +215,7 @@
 
             $('#search').on('keyup', function () {
                 let search = $(this).val().trim();
-                if (search.length > 3) {
+                if (search.length > 3 || search.length === 0) {
                     get_data(1);
                 }
             });
